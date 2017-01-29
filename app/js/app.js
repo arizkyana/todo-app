e226c75827bcb22e2d17005b6ca9f165eecf6262
agung <agung.rizkyana@gmail.com>
2017-01-29 15:18:34 +0700

    remove belum jalan

--- app/js/app.js
+++ app/js/app.js
@@ -8,39 +8,84 @@ app.run(['$rootScope', '$log', runTime]);
 app.factory('TodoFactory', ['$http', '$log', TodoFactory]);
 
 // controller
-app.controller('TodoController', ['$scope', '$rootScope', 'TodoFactory', TodoController]);
+app.controller('TodoController', ['$scope', '$rootScope', 'TodoFactory', '$log', TodoController]);
 
+// util
+app.filter('reverse', reverse);
 
+/**
+ * @name reverse
+ * @desc handle array reverse
+ */
+function reverse() {
+  return function (items) {
+    return items.slice().reverse();
+  };
+}
 
+/**
+ * @name runTime
+ * @param D.I $rootScope, $log
+ * @desc handle Angular Runtime
+ */
 function runTime($rootScope, $log) {
   $log.log("TODO App Running with Angular");
 }
 
-
-function TodoController($scope, $rootScope, TodoFactory) {
+/**
+ * @name TodoController
+ * @param D.I $rootScope, $log, TodoFactory
+ * @desc handle Active Screen and Data Presentation
+ */
+function TodoController($scope, $rootScope, TodoFactory, $log) {
   $scope.data = {
     todo: ''
   };
 
-  $scope.todos = [];  
+  $scope.todos = [];
+
+  $scope.onChangeIsDone = function onChangeIsDone() { };
+  $scope.onClickTodoEdit = function onClickTodoEdit(index) {
+    $scope.todos[index].isEdit = true;
+
+    // Update data 
+    TodoFactory.todos = $scope.todos;
+  };
+  $scope.onClickTodoRemove = function onClickTodoRemove(index) {
+    $scope.todos.splice(index, 1);
+    delete $scope.todos[index];
+    $log.warn($scope.todos);
+  };
+  $scope.onClickTodoSaveEdit = function onClickTodoSaveEdit(index) {
+    $scope.todos[index].isEdit = false;
+    $scope.todos[index].name = $scope.todos[index].editedName;
+
+    // Update data 
+    TodoFactory.todos = $scope.todos;
+  };
 
   $scope.addTodo = function addTodo() {
     TodoFactory.add($scope.data.todo);
     $scope.todos = angular.copy(TodoFactory.todos);
-    
+    $scope.data.todo = "";
   };
 }
 
+/**
+ * @name TodoFactory
+ * @param D.I $http, $log
+ * @desc handle Data Application
+ */
 function TodoFactory($http, $log) {
 
-  var todos = [];  
+  var todos = [];
 
   var add = function (todo) {
     $log.log(todo);
     todos.push({
-      name : todo
+      name: todo
     });
-  };  
+  };
 
   return {
     add: add,
