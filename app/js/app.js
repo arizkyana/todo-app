0c3f70492cb0a07b2660e319f70098465d449d9e
agung <agung.rizkyana@gmail.com>
2017-01-29 07:14:20 +0700

    perbaikan TodoFactory dan repeat

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,49 @@
+// init angular app for todo
 var app = angular.module('todo', []);
 
-app.run(function () {
-  console.log("TODO App Running with Angular");
-});
+// runtime 
+app.run(['$rootScope', '$log', runTime]);
+
+// factory / model
+app.factory('TodoFactory', ['$http', '$log', TodoFactory]);
+
+// controller
+app.controller('TodoController', ['$scope', '$rootScope', 'TodoFactory', TodoController]);
+
+
+
+function runTime($rootScope, $log) {
+  $log.log("TODO App Running with Angular");
+}
+
+
+function TodoController($scope, $rootScope, TodoFactory) {
+  $scope.data = {
+    todo: ''
+  };
+
+  $scope.todos = [];  
+
+  $scope.addTodo = function addTodo() {
+    TodoFactory.add($scope.data.todo);
+    $scope.todos = angular.copy(TodoFactory.todos);
+    
+  };
+}
+
+function TodoFactory($http, $log) {
+
+  var todos = [];  
+
+  var add = function (todo) {
+    $log.log(todo);
+    todos.push({
+      name : todo
+    });
+  };  
+
+  return {
+    add: add,
+    todos: todos
+  };
+}
