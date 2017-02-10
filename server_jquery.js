7eefb819d5bd334dd954e7fd9f78fff87d4eb866
agung <agung.rizkyana@gmail.com>
2017-02-10 11:26:45 +0700

    update server runner untuk jquery

--- server_jquery.js
+++ server_jquery.js
@@ -3,10 +3,10 @@ var http = require('http');
 var path = require('path');
 var app = express();
 
-app.use(express.static( __dirname + '/app' ));
+app.use(express.static( __dirname + '/app_jquery' ));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'app', 'index.html'));
+  res.sendFile(path.join(__dirname, 'app_jquery', 'index.html'));
 });
 
 app.listen(3000, function () {
